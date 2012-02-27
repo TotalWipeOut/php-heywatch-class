f2a4dde18693c53320a39efa6e161386555c695b
kevin <kevin@192.168.3.31>
2012-02-27 16:44:56 +0000

    Added HeyWatch::responseFormat to enable option of xml and json responses. Fixed incorrect variable name in getFormat()

--- heywatch.php
+++ heywatch.php
@@ -25,7 +25,7 @@ class HeyWatch {
         300 => 'No more credit available',
         301 => 'Not enough credits'
     );
-    
+
     var $responses = array(
         200 => 'Response OK',
         201 => 'The resource has been created successfully',
@@ -34,9 +34,16 @@ class HeyWatch {
         404 => 'The resource you requested is not available',
         406 => 'The format you asked is not available'
     );
-    
+
     var $log = '';
-    
+
+    /**
+     * The data format for HeyWatch reponses
+     *
+     * json or xml
+     **/
+    var $responseFormat = 'xml';
+
     /**
      * The HeyWatch API credentials
      **/
@@ -46,19 +53,19 @@ class HeyWatch {
      * The HeyWatch API Location
      **/
     static $api_location = 'http://heywatch.com/';
-    
+
     /**
      * The construct for this API
      *
-     * @param string $username 
-     * @param string $password 
+     * @param string $username
+     * @param string $password
      * @return void
      * @author Andreas Creten
      */
     function __construct($username, $password) {
         $this->auth = $username.':'.$password;
     }
-    
+
     /**
      * Get all the encoded videos
      *
@@ -68,24 +75,24 @@ class HeyWatch {
     function getEncodedVideos() {
         return $this->api_call('encoded_video');
     }
-    
+
     /**
      * Get one encoded video
      *
-     * @param integer $encoded_video_id 
+     * @param integer $encoded_video_id
      * @return SimpleXMLObject The video
      * @author Andreas Creten
      */
     function getEncodedVideo($encoded_video_id) {
         return $this->api_call('encoded_video/'.$encoded_video_id);
     }
-    
+
     /**
      * Get the thumbnail url for an encoded video
      *
-     * @param integer $encoded_video_id 
-     * @param string $height 
-     * @param string $width 
+     * @param integer $encoded_video_id
+     * @param string $height
+     * @param string $width
      * @param string $seconds Offset in second (position in the video)
      * @return string The video url
      * @author Andreas Creten
@@ -96,13 +103,13 @@ class HeyWatch {
         $url .= $width ? '&width='.$width : '';
         return $url;
     }
-    
+
     /**
      * Get the thumbnail data for an encoded video
      *
-     * @param integer $encoded_video_id 
-     * @param string $height 
-     * @param string $width 
+     * @param integer $encoded_video_id
+     * @param string $height
+     * @param string $width
      * @param string $seconds Offset in second (position in the video)
      * @return string The video data
      * @author Andreas Creten
@@ -111,7 +118,7 @@ class HeyWatch {
         $url = $this->getEncodedVideoThumbnail($encoded_video_id, $height = null, $width = null, $seconds = 1);
         return file_get_contents(str_replace('http://', 'http://'.$this->auth.'@', $url));
     }
-    
+
     /**
      * Update an encoded video
      *
@@ -123,18 +130,18 @@ class HeyWatch {
     function updateEncodedVideo($encoded_video_id, $title) {
         return $this->api_call('encoded_video/'.$encoded_video_id, 'put', array('title' => $title));
     }
-    
+
     /**
      * Delete an encoded video
      *
-     * @param integer $encoded_video_id 
+     * @param integer $encoded_video_id
      * @return boolean
      * @author Andreas Creten
      */
     function deleteEncodedVideo($encoded_video_id) {
         return $this->api_call('encoded_video/'.$encoded_video_id, 'delete');
     }
-    
+
     /**
      * Get all the videos
      *
@@ -144,18 +151,18 @@ class HeyWatch {
     function getVideos() {
         return $this->api_call('video');
     }
-    
+
     /**
      * Get one video
      *
-     * @param integer $video_id 
+     * @param integer $video_id
      * @return SimpleXMLObject The video
      * @author Andreas Creten
      */
     function getVideo($video_id) {
         return $this->api_call('video/'.$video_id);
     }
-    
+
     /**
      * Update a video
      *
@@ -167,18 +174,18 @@ class HeyWatch {
     function updateVideo($video_id, $title) {
         return $this->api_call('video/'.$video_id, 'put', array('title' => $title));
     }
-    
+
     /**
      * Delete a video
      *
-     * @param integer $video_id 
+     * @param integer $video_id
      * @return boolean
      * @author Andreas Creten
      */
     function deleteVideo($video_id) {
         return $this->api_call('video/'.$video_id, 'delete');
     }
-    
+
     /**
      * Get the list of all the formats
      *
@@ -188,18 +195,18 @@ class HeyWatch {
     function getFormats() {
         return $this->api_call('format');
     }
-    
+
     /**
      * Retrieve information about the format ID
      *
-     * @param integer $format_id 
+     * @param integer $format_id
      * @return SimpleXMLObject The format
      * @author Andreas Creten
      */
     function getFormat($format_id) {
-        return $this->api_call('download/'.$download_id);
+        return $this->api_call('download/'.$format_id);
     }
-    
+
     /**
      * Create a format
      *
@@ -210,7 +217,7 @@ class HeyWatch {
      */
     function createFormat($name, $optionals = array()) {
         $args = array_merge(array('name' => $name), $optionals);
-        
+
         /*
         category: format category; can be formats, devices—Default is ‘formats’
         container: video format; can be avi, mp4, 3gp, psp, mpeg, mpegts, dvd, flv, svcd, vcd, vob, asf, mov, rm, mjpeg, mpeg2video, 3g2, mp3, ogg
@@ -224,22 +231,22 @@ class HeyWatch {
         audio_bitrate: Audio bitrate in kbit/s, up to 512kbits—Not necessary if audio_channel is 0
         sample_rate: Sample rate; can be 8000, 11025, 16000, 22000, 22050, 24000, 32000, 44000, 44100, 48000—Not necessary if audio_channel is 0
         two_pass: 2-pass encoding option; can be true or false
-        
+
         */
         return $this->api_call('format', 'post', $args);
     }
-    
+
     /**
      * Destroy a format
      *
-     * @param integer $format_id 
+     * @param integer $format_id
      * @return boolean
      * @author Andreas Creten
      */
     function deleteFormat($format_id) {
         return $this->api_call('format/'.$format_id, 'delete');
     }
-    
+
     /**
      * Get the list of all the jobs
      *
@@ -249,23 +256,23 @@ class HeyWatch {
     function getJobs() {
         return $this->api_call('job');
     }
-    
+
     /**
      * Get one job
      *
-     * @param integer $job_id 
+     * @param integer $job_id
      * @return SimpleXMLObject The job
      * @author Andreas Creten
      */
     function getJob($job_id) {
         return $this->api_call('job/'.$job_id);
     }
-    
+
     /**
      * Create a job
      *
      * @param integer $video_id
-     * @param integer $format_id 
+     * @param integer $format_id
      * @param array $optionals
      * @return boolean
      * @author Andreas Creten
@@ -274,23 +281,23 @@ class HeyWatch {
         $args = array_merge(array('video_id' => $video_id, 'format_id' => $format_id), $optionals);
         return $this->api_call('job', 'post', $args);
     }
-    
+
     /**
      * Cancel a job
      *
-     * @param integer $job_id 
+     * @param integer $job_id
      * @return boolean
      * @author Andreas Creten
      */
     function cancelJob($job_id) {
         return $this->api_call('job/'.$job_id, 'delete');
     }
-    
+
     /**
      * Upload a file
      *
-     * @param string $file 
-     * @param string $optionals 
+     * @param string $file
+     * @param string $optionals
      * @return boolean
      * @author Andreas Creten
      */
@@ -302,10 +309,10 @@ class HeyWatch {
         max_length: length in second. If you don’t want your users upload 45min of video
         custom fields: if you activate the ping after transfer, you will receive the custom fields in the request
         */
-        
+
         return $this->api_call('upload', 'post', $args);
     }
-    
+
     /**
      * Get the list of all the downloads
      *
@@ -315,23 +322,23 @@ class HeyWatch {
     function getDownloads() {
         return $this->api_call('download');
     }
-    
+
     /**
      * Get one download
      *
-     * @param integer $download_id 
+     * @param integer $download_id
      * @return SimpleXMLObject The download
      * @author Andreas Creten
      */
     function getDownload($download_id) {
         return $this->api_call('download/'.$download_id);
     }
-    
+
     /**
      * Create a download
      *
      * @param integer $video_id
-     * @param string $url 
+     * @param string $url
      * @param array $optionals
      * @return boolean
      * @author Andreas Creten
@@ -340,18 +347,18 @@ class HeyWatch {
         $args = array_merge(array('url' => $url), $optionals);
         return $this->api_call('download', 'post', $args);
     }
-    
+
     /**
      * Delete a download
      *
-     * @param integer $video_id 
+     * @param integer $video_id
      * @return boolean
      * @author Andreas Creten
      */
     function deleteDownload($download_id) {
         return $this->api_call('download/'.$download_id, 'delete');
     }
-    
+
     /**
      * Get all info about your account
      *
@@ -361,13 +368,13 @@ class HeyWatch {
     function account() {
         return $this->api_call('account');
     }
-    
+
     /**
      * Do a call to the heywatch API
      *
      * @param string $url The url to be called
      * @param string $method The method to be called
-     * @param array $arguments The arguments for this call 
+     * @param array $arguments The arguments for this call
      * @return SimpleXMLObject The heywatch response
      * @author Andreas Creten
      **/
@@ -377,14 +384,14 @@ class HeyWatch {
         $ch = curl_init();
 
         // Set the target url
-        curl_setopt($ch, CURLOPT_URL, self::$api_location.$url.'.xml');
-        
+        curl_setopt($ch, CURLOPT_URL, self::$api_location.$url.'.'.$this->responseFormat);
+
         // Enable return transfer
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        
+
         // Specify api credentials
         curl_setopt($ch, CURLOPT_USERPWD, $this->auth);
-        
+
         if($method != 'post') {
             // Set a custom request
             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, strtoupper($method));
@@ -393,25 +400,25 @@ class HeyWatch {
             // Enable post
             curl_setopt($ch, CURLOPT_POST, 1);
         }
-        
+
         if(!empty($arguments)) {
             // Add the arguments to curl
             curl_setopt($ch, CURLOPT_POSTFIELDS, $arguments);
         }
-        
+
         // Execute the curl request
         $result = curl_exec($ch);
-        
+
         if(!curl_errno($ch)) {
             // Get the http status code
             $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         }
-        
+
         // Close curl
         curl_close($ch);
-        
+
         $this->log('in', $http_status.' '.$this->responses[$http_status]);
-        
+
         if(isset($http_status)) {
             if($http_status == 202 && $method == 'post') {
                 return true;
@@ -420,22 +427,29 @@ class HeyWatch {
                 return true;
             }
         }
-        
+
         if(!empty($result)) {
-            // Parse the response as an SimpleXML object
-            $xml = simplexml_load_string($result);
-            
-            if(isset($xml['status']) && $xml['status'] == 'fail') {
+
+            if($this->responseFormat=='xml') {
+                // Parse the response as an SimpleXML object
+                $data = simplexml_load_string($result);
+            }
+            else {
+                // Parse the response as an JSON object
+                $data = json_decode($result, true);
+            }
+
+            if(isset($data['status']) && $data['status'] == 'fail') {
                 return false;
             }
-            
+
             // Return the response
-            return $xml;
+            return $data;
         }
-        
+
         return false;
     }
-    
+
     private function log($direction, $string) {
         if($direction == 'out') {
             $direction = 'PUT > ';
